4547f1fbc9d6b4babcbd1b159003986ecaed1398
Pan <zuboci@yandex.com>
2018-07-08 21:37:43 +0100

    Cleanup

--- ci/travis/pyenv-wheel.sh
+++ ci/travis/pyenv-wheel.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
 brew install pyenv || brew outdated pyenv || brew upgrade pyenv
 
@@ -6,14 +6,21 @@ export PYENV_VERSION=$PYENV
 if [[ ! -d "$HOME/.pyenv/versions/$PYENV_VERSION" ]]; then
     pyenv install $PYENV_VERSION
 fi
-export PYENV_ROOT="$HOME/.pyenv"
 pyenv global $PYENV_VERSION
 pyenv versions
+
+set +x
 eval "$(pyenv init -)"
+set -x
+
 which python
 python -m pip install -U virtualenv
 virtualenv -p "$(which python)" venv
+
+set +x
 source venv/bin/activate
+set -x
+
 python -V
 python -m pip install -U setuptools pip
 pip install -U delocate wheel
@@ -23,12 +30,15 @@ cp /usr/local/lib/libssh2* .
 delocate-listdeps --all *.whl
 delocate-wheel -v *.whl
 delocate-listdeps --all *.whl
+
 ls -l *.whl
 rm -f *.dylib
 pip install -v *.whl
 pwd; mkdir -p temp; cd temp; pwd
-python -c "import pssh.clients"
+python -c "import pssh.clients" && echo "Import successfull"
 cd ..; pwd
+set +x
 deactivate
+set -x
 
 mv -f *.whl wheels/
